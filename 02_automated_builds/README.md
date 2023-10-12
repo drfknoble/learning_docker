c767102dadca6b6918acca3759599d5a1e63ac3a
drfknoble <frazer.noble@gmail.com>
2023-10-12 09:20:50 +0000

    Added a second project and tweaked some of the files.

---
+++ 02_automated_builds/README.md
@@ -0,0 +1,2 @@
+# 02_automated_builds
+
---
+++ 02_automated_builds/src/build/CMakeFiles/automated_myapp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/automated_myapp.dir/main.cpp.o"
+  "automated_myapp"
+  "automated_myapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/automated_myapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
